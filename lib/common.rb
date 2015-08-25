e51ea6c70681c34dd2cbd48cf36a79a180f974ab
Ben Visness <bvisness@gmail.com>
2015-08-25 15:51:36 -0500

    Switch to using blocks for input instead of strings

--- lib/common.rb
+++ lib/common.rb
@@ -11,13 +11,23 @@ class Common < Middleman::Extension
   end
 
   helpers do
-    def aside(content, options={})
+
+    # Creates an aside with markdown formatting. Because of stupid problems
+    # with ERB, this cannot be echoed normally, and handles output itself instead.
+    # So you have to call it with <%= this syntax %>, instead of <% this %>.
+    def aside(options={})
+      # ERB is terrible, so we have to "capture" the
+      # contents of the block we just passed in.
+      # http://apidock.com/rails/ActionView/Helpers/CaptureHelper/capture
+      content = capture do yield end
+
       asideClass = 'notice'
       if options[:class]
         asideClass = options[:class]
       end
 
-      "<aside class=\"#{asideClass}\">#{@markdown.render(content)}</aside>"
+      concat "<aside class=\"#{asideClass}\">#{@markdown.render(content)}</aside>"
     end
+    
   end
 end
