fd10dbdf61e269e7b2f828debc8ca6ad747b312d
Ben Visness <bvisness@gmail.com>
2016-01-12 09:15:26 -0600

    Clean up common.rb

--- lib/common.rb
+++ lib/common.rb
@@ -1,6 +1,7 @@
 require 'json'
 
 class Common < Middleman::Extension
+
   def initialize(app, options_hash={}, &block)
     super
     @objects = {}
@@ -13,31 +14,26 @@ class Common < Middleman::Extension
   end
 
   helpers do
-
     # Creates an aside with markdown formatting. Because of stupid problems
     # with ERB, this cannot be echoed normally, and handles output itself instead.
-    # So you have to call it with <%= this syntax %>, instead of <% this %>.
+    # So you have to call it with <% this syntax %>, instead of <%= this %>.
     def aside(options={})
       # ERB is terrible, so we have to "capture" the
       # contents of the block we just passed in.
       # http://apidock.com/rails/ActionView/Helpers/CaptureHelper/capture
       content = capture do yield end
 
-      asideClass = 'notice'
-      if options[:class]
-        asideClass = options[:class]
-      end
-
+      asideClass = options[:class] || 'notice'
       concat "<aside class=\"#{asideClass}\">#{@markdown.render(content)}</aside>"
     end
 
     def json(options={})
-      indent = options[:indent] || 2
+      indentWidth = options[:indent] || 2
 
       content = capture do yield end
       output = ""
 
-      formattedJSON = JSON.pretty_generate(JSON.parse(content), indent: ' ' * indent)
+      formattedJSON = JSON.pretty_generate(JSON.parse(content), indent: ' ' * indentWidth)
 
       output << "```json\n"
       output << formattedJSON
@@ -66,4 +62,5 @@ class Common < Middleman::Extension
     end
     
   end
+
 end
